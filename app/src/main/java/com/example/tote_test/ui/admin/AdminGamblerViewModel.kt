5d4f2469bd2c182ae3fb568fecfd7c57ac553924
YurijM <y-myagkov@mail.ru>
2022-10-16 12:05:17 +0300

    Сохранение данных на странице администрирования участника

---
+++ app/src/main/java/com/example/tote_test/ui/admin/AdminGamblerViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.tote_test.ui.admin
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tote_test.models.GamblerModel
+import com.example.tote_test.utils.REPOSITORY
+import com.example.tote_test.utils.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class AdminGamblerViewModel : ViewModel() {
+    private val _status = MutableLiveData<Resource<Boolean>>()
+    val status: LiveData<Resource<Boolean>> = _status
+
+    fun saveProfile(gambler: GamblerModel) {
+        _status.postValue(Resource.Loading())
+
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = REPOSITORY.saveGambler(gambler.id, gambler).data ?: false
+
+            _status.postValue(Resource.Success(result))
+        }
+    }
+
+}
